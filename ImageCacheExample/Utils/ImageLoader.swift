c9fae24fb2de0b1175b0ecc22429746f4eeee431
Jaewon Yun <woin2ee@gmail.com>
2022-11-12 21:28:20 +0900

    인 메모리 캐시 구현

--- ImageCacheExample/Utils/ImageLoader.swift
+++ ImageCacheExample/Utils/ImageLoader.swift
@@ -10,6 +10,8 @@ import Alamofire
 
 struct ImageLoader {
     
+    static private var cacheImages: [URLRequest : UIImage] = [:]
+    
     static func patch(_ urlString: String) async -> UIImage? {
         guard let url = URL.init(string: urlString) else { return nil }
         return await patch(url)
@@ -20,11 +22,22 @@ struct ImageLoader {
     }
     
     static func patch(_ urlRequest: URLRequest) async -> UIImage? {
-        guard let imageData = try? await AF.request(urlRequest)
+        if let image = cacheImages[urlRequest] {
+            return image
+        }
+        
+        let session: Session = .init(configuration: .ephemeral)
+        guard let imageData = try? await session.request(urlRequest)
             .serializingData()
             .value
         else { return nil }
         
-        return UIImage.init(data: imageData)
+        guard let image = UIImage.init(data: imageData) else {
+            return nil
+        }
+        
+        cacheImages[urlRequest] = image
+        
+        return image
     }
 }
